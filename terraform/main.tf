09f96ad12d89bf83d9f5a6b51648ca1d4ba221aa
Aleksandr Kostyrev <kostyrevaa@gmail.com>
2017-11-08 17:34:15 +0300

    Use r4.4xlarge and 500Gb ebs volume for data

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,7 +102,7 @@ resource "aws_key_pair" "ssh-key" {
 
 resource "aws_spot_instance_request" "anaconda" {
   ami                  = "${data.aws_ami.anaconda.id}"
-  instance_type        = "r4.xlarge"
+  instance_type        = "r4.4xlarge"
   spot_price           = "${var.spot_price}"
   wait_for_fulfillment = true
   spot_type            = "one-time"
@@ -111,6 +111,39 @@ resource "aws_spot_instance_request" "anaconda" {
   vpc_security_group_ids = [
     "${aws_security_group.jupyter.id}",
   ]
+
+  ebs_block_device {
+    device_name           = "/dev/xvdv"
+    volume_size           = 500
+    volume_type           = "gp2"
+    delete_on_termination = false
+  }
+
+  connection {
+    type  = "ssh"
+    user  = "ubuntu"
+    agent = "true"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/bootstrap"
+    destination = "/tmp/bootstrap"
+  }
+
+  # Without this sometimes the error arises
+  # E: Could not get lock /var/lib/dpkg/lock - open (11: Resource temporarily unavailable)
+  provisioner "remote-exec" {
+    inline = [
+      "timeout 180 /bin/bash -x -c 'until stat /var/lib/cloud/instance/boot-finished &>/dev/null; do echo Waiting for cloud-init boot-finished; sleep 6; done'",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap",
+      "sudo /tmp/bootstrap",
+    ]
+  }
 }
 
 output "public_dns" {
