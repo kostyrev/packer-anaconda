1cbd329d5dd0faad9ee17712f0abc1dfea10c46a
Aleksandr Kostyrev <kostyrevaa@gmail.com>
2018-01-10 12:41:02 +0300

    Fix code style with terraform fmt

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -26,7 +26,7 @@ resource "aws_spot_instance_request" "anaconda" {
     when = "destroy"
 
     inline = [
-      "if grep -qs '/home/ubuntu/data' /proc/mounts; then sudo umount /dev/xvdv1; fi"
+      "if grep -qs '/home/ubuntu/data' /proc/mounts; then sudo umount /dev/xvdv1; fi",
     ]
   }
 }
